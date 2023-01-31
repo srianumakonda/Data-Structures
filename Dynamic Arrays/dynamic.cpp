6d88c0d7e59df60d9d7f01690ee468ce7de8525c
Sri Anumakonda <sri.anumakonda06@gmail.com>
2023-01-30 18:58:57 -0600

    added dynamic array implementation

---
+++ Dynamic Arrays/dynamic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#define LENGTH 1000
+using namespace std;
+
+int main(){
+    int *dynArray;
+    dynArray = new int[LENGTH]; //use new keyword to define dynamic array
+    dynArray[0] = 1;
+    dynArray[1] = 2;
+    dynArray[2] = 3;
+
+    cout << "Array: ";
+    for (unsigned int i=0;i<sizeof(dynArray);i++){
+        cout << dynArray[i] << " ";
+    }
+
+    delete [] dynArray;
+    return 0;
+}
