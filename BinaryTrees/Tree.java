4d5aba530fc5687bf7eee7062d9528a3ff2cfd54
Sri Anumakonda <sri.anumakonda06@gmail.com>
2023-02-05 13:21:38 -0600

    added traversal methods for binary trees via recursion

--- BinaryTrees/Tree.java
+++ BinaryTrees/Tree.java
@@ -54,4 +54,43 @@ public class Tree {
         return false;
     }
 
+    // Overloading
+    public void preOrderTraversal(){
+        preOrderTraversal(root);
+    }
+
+    private void preOrderTraversal(Node node){
+        // root -> left -> right
+        if (node==null) return; //base condition
+        System.out.println(root.value);
+        preOrderTraversal(root.leftChild);
+        preOrderTraversal(root.rightChild);
+    }
+    
+    // Overloading
+    public void inOrderTraversal(){
+        inOrderTraversal(root);
+    }
+
+    private void inOrderTraversal(Node node){
+        // left -> root -> right
+        if (node==null) return; //base condition
+        preOrderTraversal(root.leftChild);
+        System.out.println(root.value);
+        preOrderTraversal(root.rightChild);
+    }
+
+    // Overloading
+    public void postOrderTraversal(){
+        postOrderTraversal(root);
+    }
+
+    private void postOrderTraversal(Node node){
+        // left -> root -> right
+        if (node==null) return; //base condition
+        preOrderTraversal(root.leftChild);
+        preOrderTraversal(root.rightChild);
+        System.out.println(root.value);
+    }
+
 }
