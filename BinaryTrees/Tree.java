6ae4c8bf43cf1e183eec530d9c03655ed9ea7161
Sri Anumakonda <sri.anumakonda06@gmail.com>
2023-02-05 13:29:36 -0600

    implemented height of binary tree

--- BinaryTrees/Tree.java
+++ BinaryTrees/Tree.java
@@ -93,4 +93,14 @@ public class Tree {
         System.out.println(root.value);
     }
 
+    public int height(){
+        return height(root);
+    }
+
+    private int height(Node node){
+        if (node == null) return -1;
+        if (node.leftChild == null && node.rightChild == null) return 0;
+        return 1 + Math.max(height(node.leftChild), height(node.rightChild));
+    }
+
 }
